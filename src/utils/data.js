e27e99caa833394dcd740a7d63f08a555bc76b24
user <nadalutfiyah1@gmail.com>
2022-12-31 21:57:39 +0700

    first commit

---
+++ src/utils/data.js
@@ -0,0 +1,69 @@
+import I1 from '../img/i1.png';
+import F1 from '../img/f1.png';
+import C3 from '../img/c3.png';
+import Fi1 from '../img/fi1.png';
+
+export const heroData = [
+    {
+        id : 1, 
+        name : 'Seblak', 
+        decp : 'Ceker & Telor', 
+        price : 'Rp10.000', 
+        imageSrc : I1},
+    {
+        id : 2, 
+        name : 'Nasi Goreng', 
+        decp : 'Seafood', 
+        price : 'Rp15.000', 
+        imageSrc : F1},
+    {
+        id : 3, 
+        name : 'Ayam Katsu', 
+        decp : 'Nasi & Ayam Katsu', 
+        price : 'Rp15.000', 
+        imageSrc : C3},
+    {
+        id : 4, 
+        name : 'Sop Buah', 
+        decp : 'Aneka Buah Fresh', 
+        price : 'Rp10.000', 
+        imageSrc : Fi1},
+  ];
+
+  export const categories = [
+    {
+        id : 1,
+        name : 'Chicken',
+        urlParamName : 'chicken', 
+    },
+    {
+        id : 2,
+        name : 'Curry', 
+        urlParamName : 'curry',
+    },
+    {
+        id : 3,
+        name : 'Rice', 
+        urlParamName : 'rice',
+    },
+    {
+        id : 4,
+        name : 'Fish', 
+        urlParamName : 'fish',
+    },
+    {
+        id : 5,
+        name : 'Fruits', 
+        urlParamName : 'fruits',
+    },
+    {
+        id : 6,
+        name : 'Icecreams', 
+        urlParamName : 'icecreams',
+    },
+    {
+        id : 7,
+        name : 'Soft Drinks', 
+        urlParamName : 'drinks',
+    },
+  ];
